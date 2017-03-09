6600de27b3af0620aa902319468e25be964e3ecf
Edgar Carballo <karvayoedgar@gmail.com>
2017-03-08 23:36:45 -0600

    Add doc and lava. Remove people

--- Creepy/Tiles/ITile.cs
+++ Creepy/Tiles/ITile.cs
@@ -1,19 +1,39 @@
-﻿using Microsoft.Xna.Framework;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 
 namespace Creepy
 {
+	/// <summary>
+	/// Represents an object in the tile
+	/// </summary>
 	public interface ITileObject
 	{
 		
 	}
 
+	/// <summary>
+	/// Represents a tile in the map
+	/// </summary>
 	public interface ITile
 	{
+		/// <summary>
+		/// Gets the tile location inside the map
+		/// </summary>
 		Point Location { get; }
 
-		int People { get; }
+		/// <summary>
+		/// Gets the height of this tile
+		/// </summary>
+		int Height { get; }
 
+		/// <summary>
+		/// Gets the lava height relative to the tile's height
+		/// </summary>
+		float LavaHeight { get; }
+
+		/// <summary>
+		/// Gets the collection of tile objects on this tile
+		/// </summary>
 		ICollection<ITileObject> Objects { get; }
 	}
 }
