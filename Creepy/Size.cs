c47f69d6b471d4402226de125d87850ef71a0bed
Edgar Carballo <karvayoedgar@gmail.com>
2017-03-09 16:54:29 -0600

    Add Size

---
+++ Creepy/Size.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Creepy
+{
+	public struct Size : IEquatable<Size>
+	{
+		public readonly int Height;
+		public readonly int Width;
+
+		#region IEquatable implementation
+
+		bool IEquatable<Size>.Equals (Size other)
+		{
+			throw new NotImplementedException ();
+		}
+
+		#endregion
+
+		public Size (int height, int width)
+		{
+			Height = height;
+			Width = width;
+		}
+	}
+}
