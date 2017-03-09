7c80d352dfbd88ae780c327b6e68e007f953a1db
Edgar Carballo <karvayoedgar@gmail.com>
2017-03-09 16:48:23 -0600

    Add (incomplete) struct Size and let Map have a Size

--- Creepy/Map.cs
+++ Creepy/Map.cs
@@ -1,11 +1,25 @@
-﻿using System;
+﻿using Creepy.Tiles;
 
 namespace Creepy
 {
 	public class Map
 	{
-		public Map ()
+		public readonly Size Size;
+		readonly Tile [,] tiles;
+
+		public Tile this [int x, int y]
 		{
+			get
+			{
+				// TODO; add checkers
+				return tiles [x, y];
+			}
+		}
+
+		public Map (Size size)
+		{
+			Size = size;
+			tiles = new Tile[Size.Width, Size.Height] ();
 		}
 	}
 }
