bf89de6422475999b267da3240bc592a7fb00862
Edgar Carballo <karvayoedgar@gmail.com>
2017-03-08 23:27:47 -0600

    Add tile class

---
+++ Creepy/Tiles/ITile.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+
+namespace Creepy
+{
+	public interface ITileObject
+	{
+		
+	}
+
+	public interface ITile
+	{
+		Point Location { get; }
+
+		int People { get; }
+
+		ICollection<ITileObject> Objects { get; }
+	}
+}
